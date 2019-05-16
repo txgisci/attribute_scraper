3a17394da20c6ff8e75217cd579ce38b9e8e58e2
jessica-cruz-dev <jessica.cruz.dev@gmail.com>
2019-05-15 20:46:24 -0700

    Isolated more data for db

--- attribute_scraper.py
+++ attribute_scraper.py
@@ -1,27 +1,47 @@
 # metadata Scraper
+import requests
 from requests import get
 from bs4 import BeautifulSoup
+import re
 
-img_id = 'STS099-744-38'
+# Example image id for testing purposes
+img_id = 'STS110-740-54'.split('-')
 
 # core url
 url1 = 'https://eol.jsc.nasa.gov/SearchPhotos/photo.pl?mission='
 url2 = '&roll='
 url3 = '&frame='
 
-img_list = img_id.split('-')
+
 # Formats the final url for given image id
-url = '{}{}{}{}{}{}'.format(url1, img_list[0], url2, img_list[1], url3,
-                            img_list[2])
+url = '{}{}{}{}{}{}'.format(url1, img_id[0], url2, img_id[1], url3,
+                            img_id[2])
 page = requests.get(url)
-soup = BeautifulSoup(page.content, 'html.parser')
 
-td_list = soup.find_all('td')
+soup = BeautifulSoup(page.text, 'html.parser')
 
-# loop through all table data to isolate specific metadata
-# for i in range(20, 30):
-#     print(td_list[i])
+# Pulls all data from top right table
+table_pad = soup.findAll('td',attrs={"class":"table_pad"})
 
-# index that accesses focul length of image id
-focal_length = str(td_list[21])
-print(focal_length[4:-5])
+# Photo ID
+photo_id = table_pad[1].text
+
+# Date Taken
+date_taken = table_pad[5].text[1:]
+
+# Time Taken
+time_taken = table_pad[7].text[1:]
+
+# Focal Length
+focal_length = table_pad[3].text[1:]
+
+# Pulls all data from Map Location bottom tab
+map_location = (soup.find_all('div', class_='span5')[0].get_text()).split('\n')
+
+# ISS lat long before conversion
+iss_coords = re.findall(r'\d+\.\d+', map_location[1])
+iss_direction = re.findall(r'\° (.[NSEW]?)', map_location[1])
+
+# Nadir lat long before conversion
+nadir_coords = re.findall(r'\d+\.\d+', map_location[2])
+nadir_direction = re.findall(r'\° (.[NSEW]?)', map_location[2])
